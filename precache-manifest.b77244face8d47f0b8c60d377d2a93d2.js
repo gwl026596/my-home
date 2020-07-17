712da87deddf7dca3643551259e2a4464662c9d4
wanlong.geng <2865333106@qq.com>
2020-07-17 11:04:19 +0800

    Updates

---
+++ precache-manifest.b77244face8d47f0b8c60d377d2a93d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "900a5c6c66ad7f2cb4cabae846aa20e1",
+    "url": "/my-home/index.html"
+  },
+  {
+    "revision": "cc58ca8bb7866dd716f7",
+    "url": "/my-home/static/css/main.13ef59d7.chunk.css"
+  },
+  {
+    "revision": "e28d688301acd9003f64",
+    "url": "/my-home/static/js/2.3d76cdee.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/my-home/static/js/2.3d76cdee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc58ca8bb7866dd716f7",
+    "url": "/my-home/static/js/main.6dcd946d.chunk.js"
+  },
+  {
+    "revision": "aed9109603a0bb93386c",
+    "url": "/my-home/static/js/runtime-main.b57a1af9.js"
+  }
+]);
